de614d50e8050d3f707c56d424bd13f9318e03e3
Oliver Norgate <oliverpnorgate@gmail.com>
2023-02-24 16:09:01 +0000

    made changes so that the program keeps running until a valid choice is made, removed the thrown runtime exception and allowed user to exit program

--- src/main/java/com/sparta/Scanner/DepartmentChoice.java
+++ src/main/java/com/sparta/Scanner/DepartmentChoice.java
@@ -1,21 +1,31 @@
 package com.sparta.Scanner;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class DepartmentChoice {
     public static String chooseDepartment() {
         //returns the users choice of department for use in the filter
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter the department name for which you want to search for a list of employees: ");
-        String userInput = scanner.nextLine();
-        String deptName = userInput.toLowerCase();
-        
-        if (deptName.equals("customer service") || deptName.equals("development") || deptName.equals("finance") ||
-                deptName.equals("human resources") || deptName.equals("marketing") || deptName.equals("production") ||
-                deptName.equals("quality management") || deptName.equals("research") || deptName.equals("sales")) {
-            return deptName;
-        } else {
-            throw new RuntimeException("Invalid selection, please choose a valid department name.");
+        System.out.println("This program will run until you enter the exit command 'exit");
+        boolean isRunning = true;
+
+        while(isRunning) {
+            System.out.println("Please enter the department name for which you want to search for a list of employees: ");
+            String userInput = scanner.nextLine();
+            String deptName = userInput.toLowerCase();
+            if (deptName.equals("customer service") || deptName.equals("development") || deptName.equals("finance") ||
+                    deptName.equals("human resources") || deptName.equals("marketing") || deptName.equals("production") ||
+                    deptName.equals("quality management") || deptName.equals("research") || deptName.equals("sales")) {
+                return deptName;
+            } else if(deptName.equals("exit")) {
+                System.out.println("Thank you for using our program. Goodbye.");
+                isRunning = false;
+            } else {
+                System.out.println("Invalid selection. Please choose a valid department name: ");
+            }
+
         }
+        return null;
     }
 }
