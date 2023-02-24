9114180598ada8f24d2abc944a7dc59776196c34
Jane Doe <faris.a@hotmail.co.uk>
2023-02-24 15:16:43 +0000

    updated periodChoice and test push

--- src/test/java/com/sparta/Scanner/PeriodChoiceTest.java
+++ src/test/java/com/sparta/Scanner/PeriodChoiceTest.java
@@ -1,4 +1,18 @@
+package com.sparta.Scanner;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.sql.Date;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PeriodChoiceTest {
-  
+
+    @Test
+    void choosePeriod() {
+        String[] expected = {"1987-03-22", "1999-11-2"};
+        assertArrayEquals(expected, PeriodChoice.choosePeriod());
+    }
 }
