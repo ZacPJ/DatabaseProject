98f66e9d510fc2e8f8daf7a5e71c2ffa99876395
Oliver Norgate <oliverpnorgate@gmail.com>
2023-02-24 15:55:25 +0000

    created a class that takes user input for choosing a department and returning as a string. Throws runtime exception if the input is not a valid department

--- src/main/java/com/sparta/Scanner/DepartmentChoice.java
+++ src/main/java/com/sparta/Scanner/DepartmentChoice.java
@@ -1,9 +1,21 @@
 package com.sparta.Scanner;
 
-public class DepartmentChoice {
-    public static String chooseDepartment(){
-        //returns the users choice of department for use in the filter
-        return null;
-    }
+import java.util.Scanner;
 
+public class DepartmentChoice {
+    public static String chooseDepartment() {
+        //returns the users choice of department for use in the filter
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the department name for which you want to search for a list of employees: ");
+        String userInput = scanner.nextLine();
+        String deptName = userInput.toLowerCase();
+        
+        if (deptName.equals("customer service") || deptName.equals("development") || deptName.equals("finance") ||
+                deptName.equals("human resources") || deptName.equals("marketing") || deptName.equals("production") ||
+                deptName.equals("quality management") || deptName.equals("research") || deptName.equals("sales")) {
+            return deptName;
+        } else {
+            throw new RuntimeException("Invalid selection, please choose a valid department name.");
+        }
+    }
 }
