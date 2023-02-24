41208ccc5194f0e4b9d84691af808229e34468ec
Jane Doe <faris.a@hotmail.co.uk>
2023-02-24 15:14:57 +0000

    :

--- src/main/java/com/sparta/Scanner/PeriodChoice.java
+++ src/main/java/com/sparta/Scanner/PeriodChoice.java
@@ -5,16 +5,16 @@ import java.time.LocalDate;
 import java.util.Scanner;
 
 public class PeriodChoice {
-    public static Date[] choosePeriod(){
+    public static String[] choosePeriod(){
         Scanner s = new Scanner(System.in);
         //get start date
         System.out.println("enter the start date (YYYY-mm-dd) including the dashes");
-        Date startDate = Date.valueOf(s.next());
+        String startDate = s.next();
         //get end date
         System.out.println("enter the end date (YYYY-mm-dd) including the dashes");
-        Date endDate = Date.valueOf(s.next());
+        String endDate = s.next();
         //return the two dates as an array of dates, index 0 is min date, index 1 is max date
-        return new Date[]{startDate,endDate};
+        return new String[]{startDate,endDate};
     }
 
 }
